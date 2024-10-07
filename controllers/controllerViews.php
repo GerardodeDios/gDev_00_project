f732b8ff800e96a61cd8281b9b6b5ce22fe595ee
Gerard - gDev <gerardodedios@me.com>
2024-10-07 20:53:52 +0200

    04_Controller file -> modifying in file
    04_Controller file -> modify file controllerViews.php

---
+++ controllers/controllerViews.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "./models/modelsViews.php";
+
+    class controllerViews extends modelsViews {
+
+        /*  --  Controller for getting the template  -- */
+        public function controller_get_template() {
+            return require_once "./views/template.php";
+        }
+
+        /*  --  Controller for getting the different views  -- */
+        public function controller_get_view() {
+            if (isset($_GET['views'])) {
+                $path = explode("/", $_GET['views']);
+                $response = modelsViews::model_get_view($path[0]);    # Lo hago así porque la función es de tipo static y puedo llamarla con los ::
+            }
+            else {
+                $response = "login";
+            }
+            return $response;
+        }
+    }
