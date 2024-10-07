17b9f8be5c9d357495dadcf4b514d015499bfff4
Gerard - gDev <gerardodedios@me.com>
2024-10-07 19:58:28 +0200

    01_config file -> modifico fichero app y server

--- config/app.php
+++ config/app.php
@@ -1,6 +1,15 @@
 <?php
-    const SERVERURL="http://localhost:8080/gDeveloper/gDev_PHP_00_Project/";
-    const COMPANY = "GESTINMO";
+
+    # -------------------------------------------------------------------------------------------- #
+    # This file contains the database connection and other configuration parameters
+    # Este fichero contiene la conexión a la base de datos y la configuración de otros parámetros
+    # -------------------------------------------------------------------------------------------- #
+
+    # -- Absolute Reference -- Where is located the project inside the HD / Referencia Absoluta -- Alojamiento del projecto en el Disco duro
+    const SERVERURL="http://localhost:8080/gDeveloper/gDev_00_PropMng/";
+
+    # -- Other parameters / Otros parámetros
+    const COMPANY = "PropMng";
     const COIN = "€";
 
-    date_default_timezone_set("Europe/Madrid");
+    date_default_timezone_set("Europe/Madrid");  # Configuración de la zona horaria conforme al manual de PHP
--- config/server.php
+++ config/server.php
@@ -1,12 +1,18 @@
 <?php
+    # -------------------------------------------------------------------------------------------- #
+    # This file contains the database connection and other configuration parameters
+    # Este fichero contiene la conexión a la base de datos y la configuración de otros parámetros
+    # -------------------------------------------------------------------------------------------- #
 
-    const DB = "project";
+    # -- DataBase Configuration --
+    const DB = "ilerna";
     const SERVER = "localhost";
     const USER = "root";
     const PASS = "Vanguard7##";
 
 
-    const SGBD = "mysql:host=".SERVER.";dbname=".DB;
-    const METHOD = "AES-256-CBC";
-    const SECRET_KEY = '$gPropMng@2024';
-    const SECRET_IV = '22061968';
+    # -- Sentence to connect with DB (DBMS) and Security (Hash and Encryption)
+    const DBMS = "mysql:host=".SERVER.";dbname=".DB;
+    const METHOD = "AES-256-CBC"; # Metodo para pasar las contraseñas por hash y encriptación
+    const SECRET_KEY = '$PropMng@2024';
+    const SECRET_IV = '19680622';
