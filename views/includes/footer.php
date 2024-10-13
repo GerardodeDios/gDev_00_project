d6e73f6e268906d1046127d90b82c763eeee0d01
Gerard - gDev <gerardodedios@me.com>
2024-10-13 22:23:43 +0200

    Update footer.php

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,14 +1,28 @@
-
-
-    <!--  =====  BOOTSTRAP SCRIPTS  =====  -->
-    <script src="" type="text/javascript"></script>
-
-    <!--  =====  ICONS Source: https://ionic.io/ionicons/usage   =====  -->
+<!--  =====  ICONS Source: https://ionic.io/ionicons/usage   =====  -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
     <!--  =====  BOOTSTRAP SCRIPTS  =====  -->
-    <script src="/views/js/script.js" type="text/javascript"></script>;
+    <!-- Bootstrap V4.3 -->
+    <script src="../../views/js/bootstrap.min.js" ></script>
+
+    <!-- Bootstrap Material Design V4.0 -->
+    <script src="../../views/js/bootstrap-material-design.min.js" ></script>
+    <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+    <!--  =====  JQUERY SCRIPTS  =====  -->
+    <!-- jQuery V3.4.1 -->
+    <script src="../../views/js/jquery-3.4.1.min.js" ></script>
+    <!-- jQuery Custom Content Scroller V3.1.5 -->
+    <script src="../../views/js/jquery.mCustomScrollbar.concat.min.js" ></script>
+
+    <!--  =====  POPPER SCRIPTS  =====  -->
+    <script src="../../views/js/popper.min.js" ></script>
+
+    <!--  =====  CUSTOM JAVASCRIPT SCRIPTS  =====  -->
+    <script src="../../views/js/script.js" type="text/javascript"></script>
+
 </body>
 </html>
 <?php
+?>
