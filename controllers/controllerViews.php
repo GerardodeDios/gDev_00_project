f9b405774b282fc8626eb3dc4681dbd5bc343b6b
Gerard-gDev <gerardodedios@me.com>
2024-10-13 13:08:06 +0200

    04_controllers/controllersViews -> modify comment

--- controllers/controllerViews.php
+++ controllers/controllerViews.php
@@ -13,7 +13,7 @@
         public function controller_get_view() {
             if (isset($_GET['views'])) {
                 $path = explode("/", $_GET['views']);
-                $response = modelsViews::model_get_view($path[0]);    # Lo hago así porque la función es de tipo static y puedo llamarla con los ::
+                $response = modelsViews::model_get_view($path[0]);    # I can call it by :: because it is a static function type
             }
             else {
                 $response = "login";
